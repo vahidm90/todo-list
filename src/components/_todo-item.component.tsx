a202aa5041653df5d77507dbc01ced6f5b680f4b
VahidM <49893905+vahidm90@users.noreply.github.com>
2022-09-15 17:32:29 +0200

    each item has expandable description

--- src/components/_todo-item.component.tsx
+++ src/components/_todo-item.component.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import {ITodoListItem} from "../interfaces";
 
-export class TodoItem extends React.Component<{ item: ITodoListItem, onDelete: (id: number) => void; }, any>{
+export class TodoItem extends React.Component<{ item: ITodoListItem, onDelete: (id: number) => void; }, {isExtended: boolean;}>{
+
+    constructor(props) {
+        super(props);
+        this.state = {isExtended: false};
+    }
 
     render() {
         const item = this.props.item;
+        const description = this.state.isExtended ? <p className="description">{item.description}</p> : '';
         return (
-            <span className="todo-item">
-                <span className="name" >{item.name}</span>
+            <div className="todo-item">
+                <h2 className="name" >{item.name}</h2>
                     <button className="btn btn-delete" title="remove task from list"
                             onClick={() => this.props.onDelete(item.id)}>x</button>
-            </span>
+                <button onClick={() => this.setState({isExtended: !this.state.isExtended})}>...</button>
+                {description}
+            </div>
         );
     }
 }
