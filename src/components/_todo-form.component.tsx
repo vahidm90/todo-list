f4698d7a77665559fdf2eb64fb73b2498b02e275
VahidM <49893905+vahidm90@users.noreply.github.com>
2022-09-16 10:20:00 +0200

    submit form by pressing "ctrl+Enter" inside "description" textarea;

--- src/components/_todo-form.component.tsx
+++ src/components/_todo-form.component.tsx
@@ -3,19 +3,26 @@ import {IToDo} from "../interfaces";
 
 export class TodoForm extends React.Component<any, IToDo> {
 
+    // private _isEdit!: boolean;
+    private _formRef = React.createRef<HTMLFormElement>();
+
     constructor(props) {
         super(props);
         this.state = {name: this.props.name, description: this.props.description};
         this.onFormSubmit = this.onFormSubmit.bind(this);
         this.onNameChange = this.onNameChange.bind(this);
         this.onDescriptionChange = this.onDescriptionChange.bind(this);
+        this.onTextAreaKeydown = this.onTextAreaKeydown.bind(this);
+    }
+
+    resetForm(): void {
+        this.setState({name: '', description: ''})
     }
 
     onFormSubmit(e): void {
         e.preventDefault();
-        const item = this.state;
-        this.props.onAddNewItem({name: item.name, description: item.description});
-        this.setState({name: '', description: ''})
+        this.emitData();
+        this.resetForm();
     }
 
     onNameChange(e): void {
@@ -26,14 +33,26 @@ export class TodoForm extends React.Component<any, IToDo> {
         this.setState({name: this.state.name, description: e.target.value})
     }
 
+    onTextAreaKeydown(e): void {
+        if ((e.key !== "Enter") || !e.ctrlKey)
+            return;
+        e.preventDefault();
+        this._formRef.current.requestSubmit();
+    }
+
+    emitData(): void {
+        const item = this.state;
+        this.props.onAddNewItem({name: item.name, description: item.description});
+    }
+
     render() {
 
         const item = this.state;
         return (
-            <form onSubmit={this.onFormSubmit}>
+            <form onSubmit={this.onFormSubmit} ref={this._formRef}>
                 <input type="text" placeholder="Task Name..." value={item.name} onChange={this.onNameChange} required/>
                 <textarea placeholder="Task Description..." value={item.description}
-                          onChange={this.onDescriptionChange}></textarea>
+                          onKeyDown={this.onTextAreaKeydown} onChange={this.onDescriptionChange}></textarea>
                 <button type="submit">Add</button>
             </form>
         );
